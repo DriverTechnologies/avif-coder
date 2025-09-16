6d4b3ccaa007ecc93894ad9e96c987283e45fd35
marcocarnevali <marcocarnevali@me.com>
2025-09-16 10:03:02 +0200

    jitpack support

--- avif-coder/build.gradle.kts
+++ avif-coder/build.gradle.kts
@@ -25,67 +25,10 @@
 
 @file:Suppress("UnstableApiUsage")
 
-import com.vanniktech.maven.publish.AndroidMultiVariantLibrary
-
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
-    id("signing")
-    id("com.vanniktech.maven.publish") version "0.34.0"
-}
-
-mavenPublishing {
-    if (System.getenv("PUBLISH_STATE") == "Release") {
-        signAllPublications()
-    }
-}
-
-mavenPublishing {
-    configure(
-        AndroidMultiVariantLibrary(
-            sourcesJar = true,
-            publishJavadocJar = true,
-        )
-    )
-
-    if (System.getenv("PUBLISH_STATE") == "Release") {
-        coordinates("io.github.awxkee", "avif-coder", System.getenv("VERSION_NAME") ?: "0.0.10")
-    } else {
-        coordinates("io.github.awxkee", "avif-coder", "0.0.10")
-    }
-
-    pom {
-        name.set("AVIF Coder")
-        description.set("AVIF encoder/decoder for Android")
-        inceptionYear.set("2025")
-        url.set("https://github.com/awxkee/avif-coder")
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-            }
-            license {
-                name.set("The 3-Clause BSD License")
-                url.set("https://opensource.org/license/bsd-3-clause")
-                description.set("https://opensource.org/license/bsd-3-clause")
-            }
-        }
-        developers {
-            developer {
-                id.set("awxkee")
-                name.set("Radzivon Bartoshyk")
-                url.set("https://github.com/awxkee")
-                email.set("radzivon.bartoshyk@proton.me")
-            }
-        }
-        scm {
-            url.set("https://github.com/awxkee/avif-coder")
-            connection.set("scm:git:git@github.com:awxkee/avif-coder.git")
-            developerConnection.set("scm:git:ssh://git@github.com/awxkee/avif-coder.git")
-        }
-    }
 }
 
 task("androidSourcesJar", Jar::class) {
@@ -94,8 +37,15 @@ task("androidSourcesJar", Jar::class) {
 }
 
 android {
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+
     namespace = "com.github.awxkee.avifcoder"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
@@ -108,15 +58,7 @@ android {
                         "-std=c++20",
                     )
                 )
-//                abiFilters += setOf("arm64-v8a")
-                abiFilters += setOf( "arm64-v8a", "armeabi-v7a", "x86_64", "x86")
-            }
-        }
-
-        publishing {
-            singleVariant("release") {
-                withSourcesJar()
-                withJavadocJar()
+                abiFilters += setOf("armeabi-v7a", "arm64-v8a", "x86_64", "x86")
             }
         }
 
@@ -152,6 +94,20 @@ android {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.trydriver"
+                artifactId = "avif-coder"
+                version = "1.8.0"
+            }
+        }
+    }
+}
+
 dependencies {
-    implementation("androidx.annotation:annotation-jvm:1.7.1")
+    implementation("androidx.annotation:annotation-jvm:1.9.1")
 }
